0b86fb46c4ca9b94b9442706103964710bbd4c7c
Sanmir Cruz <sanmir.cruz@gmail.com>
2021-10-27 20:03:52 -0300

    Adicionando api/config.php

---
+++ api/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/OC/teste/api/');
+define('HTTP_CATALOG', 'http://localhost/OC/teste/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/OC/teste/api/');
+define('HTTPS_CATALOG', 'http://localhost/OC/teste/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/OC/teste/api/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/OC/teste/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/OC/teste/image/');
+define('DIR_WEBHOOK', 'C:/xampp/htdocs/OC/teste/webhook/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'teste');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ocbr_');
+
